ca4ff9815e4b51e64746336e1b71704d5d6c181f
Darren Coleman <darren.j.coleman@gmail.com>
2024-01-26 10:10:59 +0000

    Added custom icon. Fixed config.

--- app.config.js
+++ app.config.js
@@ -1,16 +1,20 @@
 import 'dotenv/config'
 
 module.exports = ({config}) => {
-    config.android.config = {
-        googleMaps: {
-            apiKey: process.env.EXPO_PUBLIC_GOOGLE_API_KEY
-        }
+    config.android = {
+        config : {
+            googleMaps: {
+                apiKey: process.env.EXPO_PUBLIC_GOOGLE_API_KEY
+            }
+        },
+        package : process.env.EXPO_PACKAGE_ID
     };
-    config.android.package = process.env.EXPO_PACKAGE_ID;
+
     config.extra = {
         eas: {
             projectId: process.env.EAS_PROJECT_ID
         }
     };
+    
     return config;
 }
